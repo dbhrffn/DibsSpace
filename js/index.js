005bc6d79331ca192cabba60b11accd7fd422557
dbhrffn <dibahariffin@gmail.com>
2023-12-08 07:54:44 +0000

    First commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
-$(function(){
+
+$(document).ready(function(){
   $("#footer").load("footer.html"); 
   // Nav
   $("#nav").load("/nav/nav_bar.html");
   // Pages
   $("#intro").load("/pages/intro.html");
+  $("#hobbies").load("/pages/hobbies.html");
+  $("#skills").load("/pages/skills.html");
+  $("#furrybabies").load("/pages/furry_babies.html");
+
 });
+
+function loadContent(param_div_id){
+  document.getElementById('main').innerHTML = document.getElementById(param_div_id).innerHTML;
+};
