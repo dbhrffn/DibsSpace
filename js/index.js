8495877c5d4eac9ea0947560994ee6ab970f47a8
dbhrffn <dibahariffin@gmail.com>
2023-12-08 11:22:19 +0000

    updates

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   $("#nav").load("/nav/nav_bar.html");
   // Pages
   $("#intro").load("/pages/intro.html");
+  $("#main").load("/pages/skills.html");
   $("#hobbies").load("/pages/hobbies.html");
   $("#skills").load("/pages/skills.html");
   $("#furrybabies").load("/pages/furry_babies.html");
@@ -12,5 +13,6 @@ $(document).ready(function(){
 });
 
 function loadContent(param_div_id){
+  console.log(param_div_id);
   document.getElementById('main').innerHTML = document.getElementById(param_div_id).innerHTML;
 };
